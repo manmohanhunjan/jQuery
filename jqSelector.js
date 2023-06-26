b85bc152bdb13009c98b24702d68048f31cdd73b
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-06-26 14:56:29 +0530

    jqEffect

---
+++ jqSelector.js
@@ -0,0 +1,8 @@
+$(document).ready(()=>{
+    $("#myId"); //id selector in jQuery
+    $(".myClass"); //class selector in jQuery
+    $("div"); //element selector in jQuery
+    $("input[type='first_name']"); //attribute selector in jQuery
+    $("#contents ul.people li"); //descendant selector in jQuery
+    
+})
